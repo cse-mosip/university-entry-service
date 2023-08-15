7d8c16076f4a8755dfbdfc0f35635c710fad9dda
udith355 <udithkaushalya@gmail.com>
2023-08-15 09:58:24 +0530

    authentication part changes

--- src/main/java/com/cse19/ue/service/EntryService.java
+++ src/main/java/com/cse19/ue/service/EntryService.java
@@ -1,8 +1,10 @@
 package com.cse19.ue.service;
 
 
+import com.cse19.ue.dto.AuthResponseDto;
 import com.cse19.ue.dto.request.SaveEntryRequest;
 import com.cse19.ue.dto.response.EntranceRecordsResponse;
+import com.cse19.ue.dto.response.PersonInfo;
 import com.cse19.ue.dto.response.UserVerificationResponse;
 import com.cse19.ue.model.EntryPlace;
 import com.cse19.ue.model.EntryState;
@@ -13,6 +15,7 @@ import com.cse19.ue.repository.ExtendedEntryLogRepository;
 import com.cse19.ue.repository.EntryLogRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +31,10 @@ public class EntryService {
     private final EntryLogRepository entryLogRepository;
     private final ExtendedEntryLogRepository extendedEntryLogRepository;
     private final EntryPlaceRepository entryPlaceRepository;
+    private final PersonService personService;
+
+    @Value("${services.authentication.url}")
+    private String AUTHENTICATION_URL;
 
     public EntranceRecordsResponse entranceRecords(
             String index,
@@ -43,40 +50,45 @@ public class EntryService {
 
 
     public UserVerificationResponse saveEntryLog(SaveEntryRequest request, String subject) {
-        try {
 
-            RestTemplate restTemplate = new RestTemplate();
-            String uri = "http://localhost:7291/reg/rcapture"; //AuthServer address
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = AUTHENTICATION_URL + "/reg/rcapture"; //AuthServer address
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 
-            HttpEntity<Object> httpEntity = new HttpEntity<>(request.getBioSign(), headers);
-            ResponseEntity<?> authResult = restTemplate.exchange(uri, HttpMethod.GET,httpEntity, Object.class);
+        HttpEntity<Object> httpEntity = new HttpEntity<>(request.getBioSign(), headers);
+        ResponseEntity<AuthResponseDto> authResult = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, AuthResponseDto.class);
 
-            Object result = authResult.getBody();
+        AuthResponseDto result = authResult.getBody();
 
-            log.info("{}",result);
-            EntryPlace entryPlace = entryPlaceRepository
-                    .findById(request.getEntryPlaceId()).orElseThrow();
+        if (result.getMessage().equals("no match found"))
+            log.error("invalid user");
+        // throw exception
 
-            UniversityEntryLog universityEntryLog = UniversityEntryLog.builder()
-                    .state(EntryState.IN)
-                    .timestamp(LocalDateTime.now())
-                    .entryPlace(entryPlace)
-                    .approverEmail(subject)
-                    .build();
+        PersonInfo personInfo = personService.personInfo(result.getMessage());
 
-            entryLogRepository.save(universityEntryLog);
+        UserVerificationResponse userVerificationResponse = UserVerificationResponse.builder()
+                .name(String.format("%s %s", personInfo.getFirstName(), personInfo.getLastName()))
+                .index(personInfo.getIndex())
+                .photo("photo")
+                .build();
 
+        EntryPlace entryPlace = entryPlaceRepository
+                .findById(request.getEntryPlaceId()).orElseThrow();
 
-            return new UserVerificationResponse("kumara", "190999A", "verified");
-        } catch (Exception e) {
+        UniversityEntryLog universityEntryLog = UniversityEntryLog.builder()
+                .state(EntryState.IN)
+                .timestamp(LocalDateTime.now())
+                .entryPlace(entryPlace)
+                .approverEmail(subject)
+                .build();
 
-            return new UserVerificationResponse("kumara", "190999A", "failed");
+        entryLogRepository.save(universityEntryLog);
 
-        }
+        return userVerificationResponse;
+
     }
 
 }
